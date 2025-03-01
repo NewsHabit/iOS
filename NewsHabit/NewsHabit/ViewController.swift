540950f79c15907abfae5be468de3bce70206ac1
noeyiz <lamoureclatant@gmail.com>
2025-03-01 21:26:32 +0900

    feat: 프로젝트 생성 및 스토리보드 제거

---
+++ NewsHabit/NewsHabit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsHabit
+//
+//  Created by 지연 on 3/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
